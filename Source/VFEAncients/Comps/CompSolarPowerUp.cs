87d8b65d6d00ca2dbce64e1764fc391b5429b207
Joshua <legodude17@users.noreply.github.com>
2023-01-07 16:15:10 -0800

    Fix NRE in solar power up

--- Source/VFEAncients/Comps/CompSolarPowerUp.cs
+++ Source/VFEAncients/Comps/CompSolarPowerUp.cs
@@ -3,86 +3,85 @@ using RimWorld;
 using UnityEngine;
 using Verse;
 
-namespace VFEAncients
+namespace VFEAncients;
+
+public class CompSolarPowerUp : ThingComp
 {
-    public class CompSolarPowerUp : ThingComp
+    public static HashSet<CompPower> SolarPoweredUp = new();
+    private bool oldElectricityDisabled;
+    private CompPowerTrader powerComp;
+
+    public CompProperties_SolarPowerUp Props => props as CompProperties_SolarPowerUp;
+
+    public static bool PowerUpActive(Thing parent) => parent.Spawned && parent.Map.GameConditionManager.ElectricityDisabled;
+    public static bool PowerUpActive(CompPower powerComp) => PowerUpActive(powerComp.parent) && SolarPoweredUp.Contains(powerComp);
+
+    public override void PostSpawnSetup(bool respawningAfterLoad)
     {
-        public static HashSet<CompPower> SolarPoweredUp = new();
-        private bool oldElectricityDisabled;
-        private CompPowerTrader powerComp;
+        base.PostSpawnSetup(respawningAfterLoad);
+        powerComp = parent.TryGetComp<CompPowerTrader>();
+        if (powerComp is not null)
+            SolarPoweredUp.Add(powerComp);
+        if (parent.TryGetComp<CompPowerBattery>(out var battery)) SolarPoweredUp.Add(battery);
+    }
 
-        public CompProperties_SolarPowerUp Props => props as CompProperties_SolarPowerUp;
-
-        public static bool PowerUpActive(Thing parent) => parent.Spawned && parent.Map.GameConditionManager.ElectricityDisabled;
-        public static bool PowerUpActive(CompPower powerComp) => PowerUpActive(powerComp.parent) && SolarPoweredUp.Contains(powerComp);
-
-        public override void PostSpawnSetup(bool respawningAfterLoad)
+    public override void CompTick()
+    {
+        base.CompTick();
+        if (powerComp is null) return;
+        if (PowerUpActive(parent))
         {
-            base.PostSpawnSetup(respawningAfterLoad);
-            powerComp = parent.TryGetComp<CompPowerTrader>();
-            if (powerComp is not null)
-                SolarPoweredUp.Add(powerComp);
-            if (parent.TryGetComp<CompPowerBattery>(out var battery)) SolarPoweredUp.Add(battery);
-        }
-
-        public override void CompTick()
-        {
-            base.CompTick();
-            if (powerComp is null) return;
-            if (PowerUpActive(parent))
+            if (powerComp is CompPowerPlant plant)
             {
-                if (powerComp is CompPowerPlant plant)
-                {
-                    plant.UpdateDesiredPowerOutput();
-                    plant.PowerOutput *= Props.PowerPlantOutputMult;
-                }
-                else if (Mathf.Approximately(powerComp.PowerOutput, -powerComp.Props.basePowerConsumption))
-                {
-                    if (powerComp.PowerOutput > 0) powerComp.PowerOutput *= Props.PowerPlantOutputMult;
-                    else powerComp.PowerOutput = 0;
-                }
+                plant.UpdateDesiredPowerOutput();
+                plant.PowerOutput *= Props.PowerPlantOutputMult;
             }
-            else
+            else if (Mathf.Approximately(powerComp.PowerOutput, -powerComp.Props.basePowerConsumption))
             {
-                if (powerComp is CompPowerPlant plant) plant.UpdateDesiredPowerOutput();
-                else powerComp.SetUpPowerVars();
+                if (powerComp.PowerOutput > 0) powerComp.PowerOutput *= Props.PowerPlantOutputMult;
+                else powerComp.PowerOutput = 0;
             }
         }
-
-        public override void PostExposeData()
+        else
         {
-            base.PostExposeData();
-            Scribe_Values.Look(ref oldElectricityDisabled, "oldElectricityDisabled");
-        }
-
-        public override void PostDeSpawn(Map map)
-        {
-            SolarPoweredUp.Remove(powerComp);
-            base.PostDeSpawn(map);
+            if (powerComp is CompPowerPlant plant) plant.UpdateDesiredPowerOutput();
+            else powerComp.SetUpPowerVars();
         }
     }
 
-    public class CompProperties_SolarPowerUp : CompProperties
+    public override void PostExposeData()
     {
-        public float PowerPlantOutputMult = 1f;
-        public float WorkSpeedMult = 2f;
-
-        public CompProperties_SolarPowerUp() => compClass = typeof(CompSolarPowerUp);
+        base.PostExposeData();
+        Scribe_Values.Look(ref oldElectricityDisabled, "oldElectricityDisabled");
     }
 
-    public class StatPart_SolarPowerUp : StatPart
+    public override void PostDeSpawn(Map map)
     {
-        public override void TransformValue(StatRequest req, ref float val)
-        {
-            if (req.HasThing && req.Thing.Map.GameConditionManager.ElectricityDisabled && req.Thing.TryGetComp<CompSolarPowerUp>(out var solarPowerUp))
-                val *= solarPowerUp.Props.WorkSpeedMult;
-        }
+        SolarPoweredUp.Remove(powerComp);
+        base.PostDeSpawn(map);
+    }
+}
 
-        public override string ExplanationPart(StatRequest req)
-        {
-            if (req.HasThing && req.Thing.Map.GameConditionManager.ElectricityDisabled && req.Thing.TryGetComp<CompSolarPowerUp>(out var solarPowerUp))
-                return "VFEAncients.SolarPowerUp".Translate() + ": x" + solarPowerUp.Props.WorkSpeedMult.ToStringPercent();
-            return "";
-        }
+public class CompProperties_SolarPowerUp : CompProperties
+{
+    public float PowerPlantOutputMult = 1f;
+    public float WorkSpeedMult = 2f;
+
+    public CompProperties_SolarPowerUp() => compClass = typeof(CompSolarPowerUp);
+}
+
+public class StatPart_SolarPowerUp : StatPart
+{
+    public override void TransformValue(StatRequest req, ref float val)
+    {
+        if (req.HasThing && req.Thing.MapHeld.GameConditionManager.ElectricityDisabled && req.Thing.TryGetComp<CompSolarPowerUp>(out var solarPowerUp))
+            val *= solarPowerUp.Props.WorkSpeedMult;
+    }
+
+    public override string ExplanationPart(StatRequest req)
+    {
+        if (req.HasThing && req.Thing.MapHeld.GameConditionManager.ElectricityDisabled && req.Thing.TryGetComp<CompSolarPowerUp>(out var solarPowerUp))
+            return "VFEAncients.SolarPowerUp".Translate() + ": x" + solarPowerUp.Props.WorkSpeedMult.ToStringPercent();
+        return "";
     }
 }
